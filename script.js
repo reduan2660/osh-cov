908842f4b620e4a6c49e0839674a659796b07937
unknown <alve.reduan.09@gmail.com>
2021-09-07 01:26:10 +0600

    Input text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const select = document.getElementById("selectCountry");
-
 var countryName = select.options[select.selectedIndex].value;
+
 const span = document.getElementById("cases");
 updateCountry();
 function updateCountry() {
@@ -11,5 +11,29 @@ function updateCountry() {
     .get(`https://coronavirus-19-api.herokuapp.com/countries/${countryName}`)
     .then((data) => {
       span.innerHTML = data["data"]["cases"];
+    });
+}
+
+// ---------------------------
+// Input
+const btn = document.getElementById("updateCountryInput");
+btn.addEventListener("click", updateCountryInput);
+
+function updateCountryInput() {
+  countryName = document.getElementById("selectCountryText").value;
+  span.innerHTML = "Loading 🔃";
+
+  axios
+    .get(`https://coronavirus-19-api.herokuapp.com/countries/${countryName}`)
+    .then((data) => {
+      if (!!data["data"]["cases"]) {
+        span.innerHTML = data["data"]["cases"];
+      } else {
+        span.innerHTML = "Country doesn't exist 🌋";
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      span.innerHTML = "Error 🌋";
     });
 }
